4611f69104ef8e5e63d1faccbcb2f3fa0103be4c
niki0123456789 <chairblue07@gmail.com>
2026-07-18 15:05:29 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "equal-love-photo-manager-v106";
+const CACHE_NAME = "equal-love-photo-manager-v107";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=1.06",
-  "./js/app.js?v=1.06",
-  "./data/events.json?v=1.06",
+  "./css/style.css?v=1.07",
+  "./js/app.js?v=1.07",
+  "./data/events.json?v=1.07",
   "./data/members.json?v=1.0.0",
   "./data/positions.json?v=1.0.0-orderfix",
-  "./data/config.json?v=1.06",
+  "./data/config.json?v=1.07",
   "./manifest.webmanifest?v=1.0.0",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
